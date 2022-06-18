e4eb18f04698073ab237b45577edf826410c667e
HugoPlacer <hugoplanellmoreno@gmail.com>
2022-06-18 19:40:25 +0200

    Graphical Options

--- src/PauseMenu.gd
+++ src/PauseMenu.gd
@@ -12,10 +12,16 @@ func _ready():
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
 		self.is_paused = !is_paused
-	
-	if event.is_action_pressed("ui_cancel"):
-		self.is_paused = false
-
+	""" #això es un intent d'arreglar el bug del menu de pausa
+	if $Control/MenuPrincipal.visible:
+		if event.is_action_pressed("ui_cancel"):
+			self.is_paused = false
+	elif $Control/OptionsMenu.visible:
+		if event.is_action_pressed("ui_cancel"):
+			$Control/MenuPrincipal.show()
+			$Control/OptionsMenu.hide()
+			print($Control/MenuPrincipal/Opcions.grab_focus()) #nu se perque collons això dona error
+"""
 
 func set_is_paused(value):
 	is_paused = value
